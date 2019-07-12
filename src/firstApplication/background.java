b947b837d8b06e82e10d425c6a1aff4e06da035e
AnuragDhiman400 <anuragdhiman400@gmail.com>
2019-07-11 20:08:33 -0400

    files added

--- src/firstApplication/background.java
+++ src/firstApplication/background.java
@@ -1,17 +1,37 @@
 package firstApplication;
 
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class background {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			// changes done
 			public void run() {
@@ -29,6 +49,8 @@ public class background {
 	 * Create the application.
 	 */
 	public background() {
+		
+	
 		initialize();
 	}
 
@@ -37,8 +59,35 @@ public class background {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 797, 520);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(111, 360, 97, 25);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(346, 360, 97, 25);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(585, 360, 97, 25);
+		frame.getContentPane().add(btnNewButton_2);
+		
+		textField = new JTextField();
+		textField.setBounds(111, 155, 116, 22);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(346, 155, 116, 22);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(585, 155, 116, 22);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
 	}
-
 }
