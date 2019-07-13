ec251ac50adb083644f6a2d3d628f14b3b7b3378
AnuragDhiman400 <anuragdhiman400@gmail.com>
2019-07-12 20:52:22 -0400

    files added

--- src/firstApplication/game.java
+++ src/firstApplication/game.java
@@ -1,36 +1,55 @@
 package firstApplication;
 
-public class game {
-	public static void main(String args[]) {
-		score obj = new score();
-		int basket1 = obj.basket1();
-		int basket2 = obj.basket2();
-		int basket3 = obj.basket3();
-		System.out.println(basket1);
-		System.out.println(basket2);
-		System.out.println(basket3);
-		
-	
-	}
-} 
-//kjnknk
+//public class game {
+//	public static void main(intargs[]) {
+//		score obj = new score();
+//		intbasket1 = obj.basket1();
+//		intbasket2 = obj.basket2();
+//		intbasket3 = obj.basket3();
+//		System.out.println(basket1);
+//		System.out.println(basket2);
+//		System.out.println(basket3);
+//		
+//	
+//	}
+//} 
 
- class score {
-      
+
+ public class game {
+      public int sum=0;
+      public int sum1=0;
      public int basket1(){
     	  int outsideTheArc = 3;
-    	  return outsideTheArc;
+    	   sum = sum+outsideTheArc;
+    	  return sum;
       }
+     public int basket4(){
+   	  int outsideTheArc = 3;
+   	   sum1 = sum1+outsideTheArc;
+   	  return sum1;
+     }
       
      public int basket2(){
     	  int insideTheArc = 2;
-    	  return insideTheArc;
+    	   sum = sum+insideTheArc;
+    	  return sum;
       }
+     public int basket5(){
+   	  int insideTheArc = 2;
+   	   sum1 = sum1+insideTheArc;
+   	  return sum1;
+     }
       
      public int basket3(){
     	  int freeThrows = 1;
-    	  return freeThrows;
+    	   sum = sum+freeThrows;
+    	  return sum;
       }
+     public int basket6(){
+   	  int freeThrows = 1;
+   	   sum1 = sum1+freeThrows;
+   	  return sum1;
+     }
       
 }
 
